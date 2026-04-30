07bbae267d87c3f4045b71536d3e9410e9a45ca9
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-30 19:51:54 +0700

    menambahkan logika untuk memperbarui tautan node sebelum menghapus node pada metode hapus di kelas DoubleLinkedList

--- Doublelinked.cpp
+++ Doublelinked.cpp
@@ -116,4 +116,14 @@ public:
             // Step 3: Link previous node to next of current
             current->prev->next = current->next;
 
-            
+            // Step 4: if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+        }
+
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
+    }
+
+    
