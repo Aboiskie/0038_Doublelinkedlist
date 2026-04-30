c46b832fc13b441d8237bb88ea80316d8cd663d4
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-30 19:50:28 +0700

    menambahkan metode hapus untuk menghapus node dari DoubleLinkedList

--- Doublelinked.cpp
+++ Doublelinked.cpp
@@ -80,4 +80,12 @@ public:
         current->next = newNode; // Step 9d: current.next = newNode
     }
 
-    
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        
